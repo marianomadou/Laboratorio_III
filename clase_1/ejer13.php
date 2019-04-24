a3f85dcc62936f96bb727aca4c75a3faaf125382
marianomadou <marianomadou@gmail.com>
2019-04-24 14:59:16 -0300

    13

---
+++ clase_1/ejer13.php
@@ -0,0 +1,29 @@
+
+
+<?php
+/*
+Cargar los tres arrays con los siguientes valores y luego ‘juntarlos’ en uno. Luego mostrarlo por pantalla.
+“Perro”, “Gato”, “Ratón”, “Araña”, “Mosca”
+"1986", "1996", "2015", "78", "86"
+"php", "mysql", "html5", "typescript", "ajax"
+Para cargar los arrays utilizar la función array_push. Para juntarlos, utilizar la función array_merge.
+*/
+
+$arrayResultado=array();
+$array1=array();
+$array2=array();
+$array3=array();
+
+array_push($array1,"Perro", "Gato", "Ratón", "Araña", "Mosca");
+array_push($array2,"1986", "1996", "2015", "78", "86");
+array_push($array3,"php", "mysql", "html5", "typescript", "ajax");
+
+$arrayResultado= array_merge($array1,$array2,$array3);
+
+print_r ($arrayResultado);
+
+
+?>
+
+
+
